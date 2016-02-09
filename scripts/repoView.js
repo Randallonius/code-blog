22691b2c5416db852a19a221d4eb31b84d6ff697
Randallonius <jonathanprandall@gmail.com>
2016-02-09 13:13:15 -0800

    added in cashe function to repoView.js file

---
+++ scripts/repoView.js
@@ -0,0 +1,10 @@
+(function(module) {
+  var repoView = {};
+
+  var cashe = function () {
+    var $about = $('#about');
+
+    $about.find('ul').empty();
+    $about.show().siblings().hide();
+  };
+})(window);
